3a2f3c2391bfce013bc5de6cac6fa2553a42a18c
Olivia Zoe <zoe.i2k1@gmail.com>
2020-05-23 16:37:39 +0200

    Bump SConsStandard, implement tests, fix static linking

--- SConstruct
+++ SConstruct
@@ -8,5 +8,8 @@ env.withConan()
 env.environment['ENV']['ASAN_OPTIONS'] = 'halt_on_error=0;detect_leaks=0'
 
 # Run the executable submodule
-exe = env.SConscript("src/SConscript", variant_dir="src", duplicate = 0)
+libtaskranger = env.SConscript("src/SConscript", variant_dir="src", duplicate = 0)
 
+tests = env.SConscript("tests/SConscript", "tests", duplicate=0, exports={"libtr": libtaskranger})
+testAlias = Alias("test", [ tests ], tests[0].path)
+AlwaysBuild(testAlias)
---
+++ tests/testSources/TestFilesystemUtil.cpp
@@ -0,0 +1,23 @@
+#include "catch2/catch.hpp"
+#include <string>
+#include <regex>
+#include "taskranger/util/FilesystemUtil.hpp"
+
+/**
+ * Tests whether or not path expansion works as expected.
+ */
+TEST_CASE("Testing path expansion", "[FilesystemUtilPath]") {
+    std::string expansion = taskranger::FilesystemUtil::expandUserPath("~/.taskranger/");
+    std::string matchRegex;
+#if defined(_WIN32) || defined(_WIN64)
+    matchRegex = "(\\w:)?/users/[\\w \\.-]+/.taskranger/";
+#else
+    matchRegex = "/home/\\w+/.taskranger/";
+#endif
+    const std::regex regexObj(matchRegex, std::regex::icase);
+    std::smatch match;
+    // This is only printed if the test fails
+    INFO("System expanded to " + expansion);
+    REQUIRE(std::regex_match(expansion, match, regexObj));
+}
+
