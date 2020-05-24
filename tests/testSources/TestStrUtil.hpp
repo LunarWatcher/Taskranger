00f3e2dfb4fd12f4d414e4e3b5201f39081ea059
Olivia Zoe <zoe.i2k1@gmail.com>
2020-05-24 14:17:05 +0200

    Code coverage; add StrUtil tests

---
+++ tests/testSources/TestStrUtil.hpp
@@ -0,0 +1,35 @@
+#include "catch2/catch.hpp"
+
+#include "taskranger/util/StrUtil.hpp"
+
+TEST_CASE("Splitting", "[StrSplit]") {
+    const std::string raw = "this,is,a,string";
+    std::vector<std::string> output1 = taskranger::Util::splitString(raw, ",");
+    std::vector<std::string> output2 = taskranger::Util::splitString(raw, ",", 2);
+
+    // Test limits
+    REQUIRE(output1.size() == 4);
+    REQUIRE(output2.size() == 3);
+
+    // Verify splits
+    REQUIRE(output1.at(0) == "this");
+    REQUIRE(output1.at(3) == "string");
+    REQUIRE(output2.at(2) == "a,string");
+}
+
+TEST_CASE("Conversion", "[StrConv]") {
+    const std::vector<std::string> rawNums = {
+        "-14", "15", "77216781", "42", "997321"
+    };
+    const std::vector<unsigned long long> expected = {
+        0ul, 15ul, 77216781ul, 42ul, 997321ul
+    };
+
+    std::vector<unsigned long long> outputVector = {};
+    taskranger::Util::strVecToUll(rawNums, outputVector);
+    REQUIRE(outputVector.size() == rawNums.size());
+    for (size_t i = 0; i < outputVector.size(); i++) {
+        REQUIRE(outputVector.at(i) == expected.at(i));
+    }
+}
+
