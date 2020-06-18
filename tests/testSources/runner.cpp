1b59c08e5ccea9404828f37b9b46e9b5663dcdb9
Olivia Zoe <zoe.i2k1@gmail.com>
2020-06-18 17:09:51 +0200

    For fucks sake

--- tests/testSources/runner.cpp
+++ tests/testSources/runner.cpp
@@ -7,20 +7,19 @@
 // files, and it handles the rest of the clusterfuck setup. Really nice :D
 
 int main(int argc, const char* argv[]) {
-#if defined(_WIN32) || defined(_WIN64)
     try {
-        throw "Final attempt";
-    } catch (...) { return 0; }
-#endif
-    const static auto directory = "./tests/raw/data";
+        const static auto directory = "./tests/raw/data";
 
-    // Start test; make sure the data directory isn't polluted
-    if (std::filesystem::exists(directory)) {
-        if (!std::filesystem::remove_all(directory)) {
-            throw "ABORT! cannot delete ./tests/raw/data";
+        // Start test; make sure the data directory isn't polluted
+        if (std::filesystem::exists(directory)) {
+            if (!std::filesystem::remove_all(directory)) {
+                throw "ABORT! cannot delete ./tests/raw/data";
+            }
         }
-    }
 
-    // Run the tests
-    return Catch::Session().run(argc, argv);
+        // Run the tests
+        auto interm = Catch::Session().run(argc, argv);
+        std::cout << interm << std::endl;
+        return interm;
+    } catch (...) { return -1; }
 }
