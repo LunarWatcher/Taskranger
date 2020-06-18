d18072448254525c90211ad7f1f3471351cb3f5a
Olivia Zoe <zoe.i2k1@gmail.com>
2020-06-18 17:15:13 +0200

    Remove C++ testing, switch back to CI

--- tests/testSources/runner.cpp
+++ tests/testSources/runner.cpp
@@ -7,19 +7,16 @@
 // files, and it handles the rest of the clusterfuck setup. Really nice :D
 
 int main(int argc, const char* argv[]) {
-    try {
-        const static auto directory = "./tests/raw/data";
 
-        // Start test; make sure the data directory isn't polluted
-        if (std::filesystem::exists(directory)) {
-            if (!std::filesystem::remove_all(directory)) {
-                throw "ABORT! cannot delete ./tests/raw/data";
-            }
+    const static auto directory = "./tests/raw/data";
+
+    // Start test; make sure the data directory isn't polluted
+    if (std::filesystem::exists(directory)) {
+        if (!std::filesystem::remove_all(directory)) {
+            throw "ABORT! cannot delete ./tests/raw/data";
         }
+    }
 
-        // Run the tests
-        auto interm = Catch::Session().run(argc, argv);
-        std::cout << interm << std::endl;
-        return interm;
-    } catch (...) { return -1; }
+    // Run the tests
+    return Catch::Session().run(argc, argv);
 }
