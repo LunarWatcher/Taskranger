9b460e598d05019334160deea3e17e103a4653e6
Olivia Zoe <zoe.i2k1@gmail.com>
2020-07-01 14:34:29 +0200

    Add msys override (mainly for CI)

---
+++ conanfile.py
@@ -0,0 +1,19 @@
+from conans import ConanFile
+
+import os
+
+class Taskranger(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "ycm", "scons"
+
+    default_options = {
+        "icu:data_packaging": "static",
+        "icu:shared": False
+    }
+
+    def requirements(self):
+        self.requires("nlohmann_json/3.8.0")
+        self.requires("icu/67.1")
+
+        if self.settings.os == "Windows" and "FORCE_MSYS" in os.environ:
+            self.build_requires("msys2/20190524")
