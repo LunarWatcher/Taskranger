2d200a2ec13a2090dd40627906dc0c9dfbcd4a5e
Olivia Zoe <zoe.i2k1@gmail.com>
2020-10-29 15:39:48 +0100

    Fix bad URL

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,10 +1,10 @@
 if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
-  CMAKE_URL="https://github.com/Kitware/CMake/releases/download/v3.18.4/cmake-3.18.4.tar.gz"
-  mkdir cmake && travis_retry wget -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
-  sudo rm -f $(which cmake) # Try to tell the old version to get the fuck out
-  export PATH=$(pwd)/cmake/bin:${PATH}
+    CMAKE_URL="https://cmake.org/files/v3.18/cmake-3.18.4-Linux-x86_64.tar.gz"
+    mkdir cmake && travis_retry wget -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
+    sudo rm -f $(which cmake) # Try to tell the old version to get the fuck out
+    export PATH=$(pwd)/cmake/bin:${PATH}
 else
-  brew install cmake
+    brew install cmake
 fi
 
 cmake --version
